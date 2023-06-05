c782342440d20f4aa6d562f07bef82031ecac5f0
karen0185 <kharutyunyan9@gmail.com>
2023-06-05 05:51:29 +0300

    Firs Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+import moment from 'moment';
+
+moment.updateLocale('en',{week:{dow:1}});
+const startDay = moment().startOf('month').startOf('week');
+const endDay = moment().endOf('month').endOf('week');
+
+const calendar = [];
+const day = startDay.clone();
+
+while (!day.isAfter(endDay)) {
+  calendar.push(day.clone());
+  day.add(1, 'd');
+}
+console.log(calendar);
+
+
 function App() {
   return (
     <div className="App">
