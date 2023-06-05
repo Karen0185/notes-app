0f525cd14e57ef90d1d14d2732e30519f38f407f
karen0185 <kharutyunyan9@gmail.com>
2023-06-05 07:31:57 +0300

    added calendar settings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import moment from 'moment';
 
-const App = () => {
-    
-    return(
-        <div>
-            <p>Hello WebPack</p>
-        </div>
-    );
+
+moment.updateLocale('en',{week:{dow:1}});
+const startDay = moment().startOf('month').startOf('week');
+const endDay = moment().endOf('month').endOf('week');
+
+const calendar = [];
+const day = startDay.clone();
+
+while (!day.isAfter(endDay)) {
+  calendar.push(day.clone());
+  day.add(1, 'd');
+}
+console.log(calendar);
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
 }
 
 export default App;
