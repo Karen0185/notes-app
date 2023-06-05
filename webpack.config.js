130357ec3b51066f858bc220a40ca11ef4b17fdb
karen0185 <kharutyunyan9@gmail.com>
2023-06-05 07:27:06 +0300

    config WebPack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  mode: 'development',
+    devServer: {
+        static: {
+          directory: './dist', // Заменить 'contentBase' на 'static.directory'
+        },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // путь к вашему шаблону index.html
+    }),
+  ],
+};
