42a53c7ee7ea850d0e3d5920d0d170633d2760ad
karen0185 <kharutyunyan9@gmail.com>
2023-06-11 17:27:21 +0300

    new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import moment from 'moment';
-
-
-moment.updateLocale('en',{week:{dow:1}});
-const startDay = moment().startOf('month').startOf('week');
-const endDay = moment().endOf('month').endOf('week');
-
-const calendar = [];
-const day = startDay.clone();
-
-while (!day.isAfter(endDay)) {
-  calendar.push(day.clone());
-  day.add(1, 'd');
-}
-console.log(calendar);
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
