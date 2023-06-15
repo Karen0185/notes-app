d9724d4cfb1e7649f94039959047e6f46ea26bfa
karen0185 <kharutyunyan9@gmail.com>
2023-06-15 20:54:09 +0300

    add change current month

--- src/components/CalendarPage.jsx
+++ src/components/CalendarPage.jsx
@@ -1,43 +1,49 @@
 import { useState } from 'react';
 import '../assets/styles/CalendarPage.css'
+import { BiLeftArrow, BiRightArrow } from 'react-icons/bi'
 import moment from 'moment/moment';
 
 const CalendarPage = () => {
 
     const [today, setToday] = useState(moment())
 
-    const startDay = moment().startOf('month').startOf('week')
-    const endDay = moment().endOf('month').endOf('week')
-
-    const calendar = [];
-    const day = startDay.clone();
-
     const isWeekend = true;
     const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
-
-    while(!day.isAfter(endDay)) {
-        calendar.push(day.clone())
-        day.add(1,'day')
-    }
-
-    // console.log(moment().format("DD-MM-YYYY"));
+    
+    const isSelectedMonth = (day) => today.isSame(day, 'month');
+    // const startDay = today.startOf('month').startOf('week')
+    // const endDay = today.endOf('month').endOf('week')
+    const day = today.clone().startOf('month').startOf('week').subtract(0, 'day');
+    
+    const calendar = [...Array(35)].map(() => day.add(1, 'day').clone());
+    
+    const prevHandler = () => {setToday(prev => prev.clone().subtract(1, 'month'))};
+    const todayHandler = () => {setToday(moment())};
+    const nextHandler = () => {setToday(prev => prev.clone().add(1, 'month'))};
     return(
         <div className="CalendarPage">
             <div className="wrapper">
-                <h1 className="today">{months[moment().month()]} <span>{moment().format("YYYY")}</span></h1>
+                <div className="top-panel">
+                    <h1 className="today">{months[today.month()]} <span>{moment().format("YYYY")}</span></h1>
+                    <div className="change-month">
+                        <button className='prev-month' onClick={prevHandler}><BiLeftArrow /></button>
+                        <button className='current-month' onClick={todayHandler}>Текущий</button>
+                        <button className='next-month' onClick={nextHandler}><BiRightArrow /></button>
+                    </div>
+                </div>
                     <div className="days">
                         <p>Пн</p>
                         <p>Вт</p>
                         <p>Ср</p>
                         <p>Чт</p>
                         <p>Пт</p>
-                        <p>Пт</p>
-                        <p>Вс</p>
+                        <p className='weekend'>Сб</p>
+                        <p className='weekend'>Вс</p>
                     </div>
                 <div className="calendar">
                     {
                         calendar && calendar.map((item, idx) =>
-                        <div key={idx}  className={`calendar-item ${item.format("DD-MM-YYYY") == moment().format("DD-MM-YYYY") ? 'today' : ''} ${item.day() === 6 ||item.day() === 5  ? 'isWeekend' : ''} ${item.month() !== moment().month() ? 'selected-month' : ''}`}>{item.format("D")}</div>
+                        <div key={idx}  className={`calendar-item ${item.format("DD-MM-YYYY") == moment().format("DD-MM-YYYY") ? 'today' : ''} ${item.day() === 6 ||item.day() === 0  ? 'isWeekend' : ''} ${!isSelectedMonth(item) ? 'selected-month' : ''}`}>{item.format("D")}</div>
                         )
                     }
                 </div>
